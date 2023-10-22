a8595d01330dc9c312ec092205165c5f5f28b2fb
damd2212 <53495267+damd2212@users.noreply.github.com>
2023-10-22 10:56:36 -0500

    New Relic Added

--- newrelic.js
+++ newrelic.js
@@ -2,7 +2,7 @@
 
 exports.config = {
     app_name: ['TADHack app'],
-    license_key: ['05a0af72d6bd6c611579143d0efa55a7FFFFNRAL'],
+    license_key: '05a0af72d6bd6c611579143d0efa55a7FFFFNRAL',
     logging: {
         level:'info',
     }
